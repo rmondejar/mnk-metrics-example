bb584d40499df40fb42d7ff9f00fd210d32ab0cb
Ruben Mondejar <ruben.mondejar@urv.cat>
2022-04-16 20:38:20 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-## Metrics
+## Testing Micronaut Metrics
+
+Guidelines to test our microservice locally
 
 ### Setup
 
@@ -6,10 +8,14 @@ First we need a JDK, easy to install with SDKMAN!
 
 ```sdk install java 17.0.2-tem```
 
+Second, we should run a Prometheus instance, for example
 
+```brew install prometheus```
+
+And add our microservice to the configuration:
 ```
-brew install prometheus
 vi /usr/local/etc/prometheus.yml
+
   - job_name: "micronaut"
     metrics_path: "/prometheus"
     scrape_interval: 5s
@@ -17,48 +23,55 @@ vi /usr/local/etc/prometheus.yml
     - targets: ["127.0.0.1:8080"]
 ```
 
-Last but least, Grafana
+Last but least, install Grafana, for example
+
+```brew install grafana```
+
+### Verify
+
+First we should test our microservice
+
+```./gradlew test```
+
+Expecting a proper result:
+```
+    ✔ Metrics Endpoint Successful Test
+    ✔ Prometheus Endpoint Successful Test
+    ✔ Ping Endpoint Successful Test
+    ✔ Custom Metrics Successful Test
+```
+We can also run and check our endpoints too
+
+```./gradlew run```
+
+Ping endpoint
 
 ```
-brew install grafana
+curl http://localhost:8080/ping/hello
+curl http://localhost:8080/ping/world
+...
 ```
 
-### Run
-
+Metrics endpoint
 ```
-./gradlew run
+curl http://localhost:8080/metrics
 ```
-
-```
-brew services start prometheus
-open http://localhost:9090/targets
-open http://localhost:9090/graph
-```
-
-We need to run Grafana, and our service
-```
-brew services start grafana
-open http://localhost:3000 
-    (user: admin, pwd: admin)
-
-```
-
-
-
-Some testing 
-
-```
-curl http://localhost:8080/metrics/rest
-```
-
+Result:
 ```json
-{"names":["executor","executor.active", "...","rest.ping","...","system.load.average.1m"]}
+{"names":[
+  "executor",
+  "executor.active",
+  "(...)",
+  "rest.ping",
+  "(...)",
+  "system.load.average.1m"
+]}
 ```
-
+Our custom metric endpoint
 ```
 curl http://localhost:8080/metrics/rest.ping
 ```
-
+Result:
 ```json
 {"name":"rest.ping", 
   "measurements":[{"statistic":"COUNT","value":2.0}],
@@ -68,13 +81,75 @@ curl http://localhost:8080/metrics/rest.ping
   ]
 }
 ```
-
+Prometheus endpoint
 ```
 curl http://localhost:8080/prometheus
 ```
-
+Result:
 ```
+(...)
 # HELP rest_ping_total  
 # TYPE rest_ping_total counter
-rest_ping_total{action="ping",controller="index",} 9.0
+rest_ping_total{action="ping",controller="index",} 2.0
+(...)
+```
+
+### Monitoring
+
+Now we are ready to run everything and visualize our data,
+
+#### Microservice
+If it is not alive, let's run again our microservice:
+
+```./gradlew run```
+
+#### Prometheus
+Then, let's start our prometheus and check the targets
+```
+brew services start prometheus
+open http://localhost:9090/targets
+```
+Our ping service should be there and UP:
+`http://127.0.0.1:8080/prometheus`
+
+Next is to check some metrics in the graph tab
+```
+open http://localhost:9090/graph
+    * search jvm_threads_states_threads)
+    * search rest_ping_total)
+```
+#### Grafana
+Finally, we need to run Grafana locally too
+```
+brew services start grafana
+open http://localhost:3000 
+    * set user / pwd to admin
+```
+
+Next, let's create a new data source for Prometheus
+```
+open http://localhost:3000/datasources/new
+    * set URL to http://localhost:9090
+```
+
+Then, first to visualize http successful calls to our microservice
+we need to create a board, and a new panel with the following query
+
+```
+open http://localhost:3000/dashboard/new
+    * create new board
+    * add panel
+    * set title to "HTTP Server Success"
+    * metrics browser > sum without (instance,job,exception) 
+        (increase(http_server_requests_seconds_count{status=~"2.."}[1m]))
+```
+
+Lastly to visualize our custom metric counter, we need a new panel
+and add another query
+
+```
+open http://localhost:3000/dashboard/new
+    * add panel
+    * set title to "Ping Counter"
+    * metrics browser > sum(increase(rest_ping_total[1m]))
 ```
--- src/test/kotlin/mnk/metrics/controller/PingControllerTest.kt
+++ src/test/kotlin/mnk/metrics/controller/PingControllerTest.kt
@@ -8,38 +8,136 @@ import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 
 @MicronautTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 open class PingControllerTest {
 
     @Inject
-    @field:Client("/ping")
+    @field:Client("/")
     lateinit var client: HttpClient
 
     @Test
+    @Order(1)
+    @DisplayName("Metrics Endpoint Successful Test")
+    fun verifyMetricsEndpointTest() {
+        // when:
+        val request: HttpRequest<Any> = HttpRequest.GET("/metrics")
+        val rsp = client.toBlocking().exchange(
+            request, Argument.of(Map::class.java)
+        )
+
+        // then: 'the endpoint can be accessed'
+        assertNotNull(rsp)
+        assertEquals(HttpStatus.OK, rsp.status)
+        assertNotNull(rsp.body())
+
+        // when:
+        val metrics = rsp.body()["names"] as ArrayList<*>
+
+        // then: 'expected metrics are found'
+        assertTrue(metrics.contains("jvm.memory.max"))
+        assertTrue(metrics.contains("system.cpu.usage"))
+        assertFalse(metrics.contains("rest.ping"))
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Prometheus Endpoint Successful Test")
+    fun verifyPrometheusEndpointTest() {
+        // when:
+        val request: HttpRequest<Any> = HttpRequest.GET("/prometheus")
+        val rsp = client.toBlocking().exchange(
+            request, Argument.of(String::class.java)
+        )
+        // then: 'the endpoint can be accessed'
+        assertNotNull(rsp)
+        assertEquals(HttpStatus.OK, rsp.status)
+
+        // when:
+        val body: String = rsp.body() ?: ""
+
+        // then:
+        assertTrue(body.isNotBlank())
+        assertTrue(body.contains("jvm"))
+        assertTrue(body.contains("executor"))
+        assertTrue(body.contains("http"))
+        assertFalse(body.contains("rest_ping"))
+    }
+
+    @Test
+    @Order(3)
     @DisplayName("Ping Endpoint Successful Test")
     fun verifyPingEndpointTest() {
         // given:
         val name = "test"
         // when:
-        val request: HttpRequest<Any> = HttpRequest.GET("/$name")
+        val request: HttpRequest<Any> = HttpRequest.GET("/ping/$name")
         val rsp = client.toBlocking().exchange(
             request, Argument.of(String::class.java)
         )
+        val body = rsp.body()
 
         // then: 'the endpoint can be accessed'
+        assertNotNull(rsp)
+        assertEquals(HttpStatus.OK, rsp.status)
+        assertNotNull(body)
+        assertTrue(body == "pong $name")
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Custom Metrics Successful Test")
+    fun verifyCustomMetricsTest() {
+        // when 'check the metrics endpoint again':
+        val request: HttpRequest<Any> = HttpRequest.GET("/metrics")
+        val rsp = client.toBlocking().exchange(
+            request, Argument.of(Map::class.java)
+        )
+
+        // then: 'the metrics endpoint still there'
+        assertNotNull(rsp)
         assertEquals(HttpStatus.OK, rsp.status)
         assertNotNull(rsp.body())
 
         // when:
-        val response = rsp.body()
+        val metrics = rsp.body()["names"] as ArrayList<*>
+
+        // then: 'all the metrics are found'
+        assertTrue(metrics.contains("jvm.memory.max"))
+        assertTrue(metrics.contains("system.cpu.usage"))
+        assertTrue(metrics.contains("rest.ping"))
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Prometheus Custom Metrics Successful Test")
+    fun verifyPrometheusCustomMetricsTest() {
+        // when:
+        val request: HttpRequest<Any> = HttpRequest.GET("/prometheus")
+        val rsp = client.toBlocking().exchange(
+            request, Argument.of(String::class.java)
+        )
+        // then: 'the endpoint can be accessed'
+        assertNotNull(rsp)
+        assertEquals(HttpStatus.OK, rsp.status)
+
+        // when:
+        val body: String = rsp.body() ?: ""
 
         // then:
-        assertNotNull(response)
-        assertTrue(response.equals("pong $name"))
+        assertTrue(body.isNotBlank())
+        assertTrue(body.contains("jvm"))
+        assertTrue(body.contains("executor"))
+        assertTrue(body.contains("http"))
+        assertTrue(body.contains("rest_ping"))
     }
 }
